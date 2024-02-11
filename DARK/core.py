9e7607b710b4ec9f4c8c874d1755095966e1aa2e
Omar Ashour <omar.ashour22@gmail.com>
2024-02-11 00:51:20 -0800

    Implement basic phonon material

---
+++ DARK/core.py
@@ -0,0 +1,81 @@
+import numpy as np
+from numpy import linalg as LA
+from pymatgen.core.structure import Structure
+import phonopy
+
+import DARK.constants as const
+
+class Material():
+    def __init__(self, name, structure, num_e, num_p, num_n, atomic_masses):
+        self.name = name
+        self.structure = structure
+        self.num_e = num_e
+        self.num_p = num_p
+        self.num_n = num_n
+        self.n_atoms = structure.num_sites
+
+        # Define transformation matrices
+        # Transpose is to keep up with original PhonoDark convention
+        self.real_frac_to_cart = structure.lattice.matrix.T
+        self.real_cart_to_frac = LA.inv(self.real_frac_to_cart)
+        self.recip_frac_to_cart = structure.lattice.reciprocal_lattice.matrix.T
+        self.recip_cart_to_frac = LA.inv(self.recip_frac_to_cart)
+
+class PhononMaterial(Material):
+    def __init__(self, name, phonopy_yaml_path):
+        # TODO: Need a check for when phonopy_yaml does not have NAC
+        phonopy_file = phonopy.load(phonopy_yaml=phonopy_yaml_path, is_nac=True)
+        self.phonopy_file = phonopy_file
+        n_atoms = phonopy_file.primitive.get_number_of_atoms()
+        self.n_modes = 3 * n_atoms
+
+        A = phonopy_file.primitive.get_masses() # Mass numbers
+        Z = phonopy_file.primitive.get_atomic_numbers() # Atomic numbers  
+        num_e = Z
+        num_p = Z
+        num_n = A-Z
+        atomic_masses = A * const.amu_to_eV
+
+        # NAC parameters (born effective charges and dielectric tensor)
+        self.born = np.array(phonopy_file.nac_params.get('born', np.zeros((n_atoms, 3, 3))))
+        self.epsilon = np.array(phonopy_file.nac_params.get('dielectric', np.identity(3)))
+         
+        # Create a Structure object
+        # At some point should make careful assessment of primitive vs unit_cell
+        # PhonoDark uses primitive, but what about when it's different from unit_cell?
+        positions = phonopy_file.primitive.get_scaled_positions()
+        lattice = np.array(phonopy_file.primitive.get_cell()) * const.Ang_to_inveV
+        species = phonopy_file.primitive.get_chemical_symbols()
+
+        structure = Structure(lattice, species, positions)
+
+        super().__init__(name, structure, num_e, num_p, num_n, atomic_masses)
+
+    def get_eig(self, mesh, with_eigenvectors=True):
+        # run phonopy in mesh mode 
+        self.phonopy_file.run_qpoints(mesh, with_eigenvectors=with_eigenvectors)
+
+
+        mesh_dict = self.phonopy_file.get_qpoints_dict()
+
+        eigenvectors_pre = mesh_dict.get('eigenvectors', None)
+        # convert frequencies to correct units
+        omega = const.THz_to_eV*mesh_dict['frequencies']
+
+        n_k = len(mesh)
+
+        # q, nu, i, alpha
+        # Need to reshape the eigenvectors from (n_k, n_modes, n_modes) 
+        # to (n_k, n_atoms, n_modes, 3)
+        eigenvectors = np.zeros((len(mesh), self.n_modes, self.n_atoms, 3), dtype=complex)
+        # Should rewrite this with a reshape...
+        for q in range(n_k):
+            for nu in range(self.n_modes):
+                eigenvectors[q,nu] = np.array_split(
+                        eigenvectors_pre[q].T[nu], self.n_atoms)
+
+        return omega, eigenvectors
+    
+class MagnonMaterial(Material):
+    def __init__(self):
+        print("Not implemented yet!")
