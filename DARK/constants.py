e5e4f72521e10ff15f20bf3ab8f89de6ec4c7669
Omar Ashour <omar.ashour22@gmail.com>
2024-02-11 15:31:03 -0800

    Linting

--- DARK/constants.py
+++ DARK/constants.py
@@ -1,6 +1,7 @@
 """
     Constants in natural units (unless otherwise specified).
 """
+
 from scipy.special import erf
 import numpy as np
 from numpy import pi
@@ -16,23 +17,23 @@ invcm_to_eV = invMeter_to_eV * 1e2
 invAng_to_eV = invMeter_to_eV * 1e10
 eV_to_invAng = 1 / invAng_to_eV
 inveV_to_Ang = invAng_to_eV
-Ang_to_inveV = 1/inveV_to_Ang
+Ang_to_inveV = 1 / inveV_to_Ang
 
 # mass
-kg_to_eV = constants.c**2/constants.e
-amu_to_eV = constants.physical_constants['atomic mass constant'][0] * kg_to_eV
+kg_to_eV = constants.c**2 / constants.e
+amu_to_eV = constants.physical_constants["atomic mass constant"][0] * kg_to_eV
 
 # Energy and frequency
-Hz_to_eV = constants.h/constants.e
-s_to_inveV = 1/Hz_to_eV
+Hz_to_eV = constants.h / constants.e
+s_to_inveV = 1 / Hz_to_eV
 THz_to_eV = Hz_to_eV * 1e12
-year_to_s = 365.25*24*60*60
-kg_year = year_to_s*s_to_inveV*kg_to_eV
+year_to_s = 365.25 * 24 * 60 * 60
+kg_year = year_to_s * s_to_inveV * kg_to_eV
 
 # Some constants
-theta_earth = 42 * (pi/180) # Angle of north pole relative to earth velocity
-rho_chi = 0.3e9 * invcm_to_eV**3 # DM Energy Density (1/eV^2)
-alpha_FS = constants.alpha # Fine structure constant
+theta_earth = 42 * (pi / 180)  # Angle of north pole relative to earth velocity
+rho_chi = 0.3e9 * invcm_to_eV**3  # DM Energy Density (1/eV^2)
+alpha_FS = constants.alpha  # Fine structure constant
 
 # Masses
 m_p = constants.m_p * kg_to_eV
@@ -41,9 +42,13 @@ m_e = constants.m_e * kg_to_eV
 
 # Maxwell-Boltzmann Distribution
 # These are the values in the magnon paper
-V0 = 220e3/constants.c # Dispersion
-VE = 240e3/constants.c # Earth velocity
-VESC = 500e3/constants.c # Galactic escape velocity
-N0 = pi**(3/2)*V0**2*(V0*erf(VESC/V0) - (2/np.sqrt(pi))*VESC*np.exp(-VESC**2/V0**2))
-C1 = pi*V0**2/N0
-C2 = np.exp(-(VESC/V0)**2)
+V0 = 220e3 / constants.c  # Dispersion
+VE = 240e3 / constants.c  # Earth velocity
+VESC = 500e3 / constants.c  # Galactic escape velocity
+N0 = (
+    pi ** (3 / 2)
+    * V0**2
+    * (V0 * erf(VESC / V0) - (2 / np.sqrt(pi)) * VESC * np.exp(-(VESC**2) / V0**2))
+)
+C1 = pi * V0**2 / N0
+C2 = np.exp(-((VESC / V0) ** 2))
--- DARK/core.py
+++ DARK/core.py
@@ -6,7 +6,8 @@ from radtools import MagnonDispersion
 
 import DARK.constants as const
 
-class Material():
+
+class Material:
     def __init__(self, name, structure, num_e, num_p, num_n, m_atoms):
         self.name = name
         self.structure = structure
@@ -26,6 +27,7 @@ class Material():
         self.m_cell = np.sum(m_atoms)
         self.xj = structure.cart_coords
 
+
 class PhononMaterial(Material):
     def __init__(self, name, phonopy_yaml_path):
         # TODO: Need a check for when phonopy_yaml does not have NAC
@@ -34,17 +36,21 @@ class PhononMaterial(Material):
         n_atoms = phonopy_file.primitive.get_number_of_atoms()
         self.n_modes = 3 * n_atoms
 
-        A = phonopy_file.primitive.get_masses() # Mass numbers
-        Z = phonopy_file.primitive.get_atomic_numbers() # Atomic numbers  
+        A = phonopy_file.primitive.get_masses()  # Mass numbers
+        Z = phonopy_file.primitive.get_atomic_numbers()  # Atomic numbers
         num_e = Z
         num_p = Z
-        num_n = A-Z
+        num_n = A - Z
         m_atoms = A * const.amu_to_eV
 
         # NAC parameters (born effective charges and dielectric tensor)
-        self.born = np.array(phonopy_file.nac_params.get('born', np.zeros((n_atoms, 3, 3))))
-        self.epsilon = np.array(phonopy_file.nac_params.get('dielectric', np.identity(3)))
-         
+        self.born = np.array(
+            phonopy_file.nac_params.get("born", np.zeros((n_atoms, 3, 3)))
+        )
+        self.epsilon = np.array(
+            phonopy_file.nac_params.get("dielectric", np.identity(3))
+        )
+
         # Create a Structure object
         # At some point should make careful assessment of primitive vs unit_cell
         # PhonoDark uses primitive, but what about when it's different from unit_cell?
@@ -60,28 +66,32 @@ class PhononMaterial(Material):
         """
         k_points: numpy arrays of k-points, fractional coordinates
         """
-        # run phonopy in mesh mode 
+        # run phonopy in mesh mode
         self.phonopy_file.run_qpoints(k_points, with_eigenvectors=with_eigenvectors)
 
         mesh_dict = self.phonopy_file.get_qpoints_dict()
 
-        eigenvectors_pre = mesh_dict.get('eigenvectors', None)
+        eigenvectors_pre = mesh_dict.get("eigenvectors", None)
         # convert frequencies to correct units
-        omega = const.THz_to_eV*mesh_dict['frequencies']
+        omega = const.THz_to_eV * mesh_dict["frequencies"]
 
         n_k = len(k_points)
 
-        # Need to reshape the eigenvectors from (n_k, n_modes, n_modes) 
+        # Need to reshape the eigenvectors from (n_k, n_modes, n_modes)
         # to (n_k, n_atoms, n_modes, 3)
-        eigenvectors = np.zeros((len(k_points), self.n_modes, self.n_atoms, 3), dtype=complex)
+        eigenvectors = np.zeros(
+            (len(k_points), self.n_modes, self.n_atoms, 3), dtype=complex
+        )
         # TODO: Should rewrite this with a reshape...
         for q in range(n_k):
             for nu in range(self.n_modes):
-                eigenvectors[q,nu] = np.array_split(
-                        eigenvectors_pre[q].T[nu], self.n_atoms)
+                eigenvectors[q, nu] = np.array_split(
+                    eigenvectors_pre[q].T[nu], self.n_atoms
+                )
 
         return omega, eigenvectors
-    
+
+
 class MagnonMaterial(Material):
     def __init__(self, name, hamiltonian, m_cell):
         """
@@ -89,11 +99,10 @@ class MagnonMaterial(Material):
         contains the magnetic atoms and their interactions.
         So m_cell needs to be specified separately
         """
-        print("Not implemented yet!")
         self.hamiltonian = hamiltonian
         n_atoms = len(hamiltonian.magnetic_atoms)
         self.n_modes = n_atoms
-        self.dispersion = MagnonDispersion(hamiltonian, phase_convention='tanner')
+        self.dispersion = MagnonDispersion(hamiltonian, phase_convention="tanner")
 
         n_atoms = len(hamiltonian.magnetic_atoms)  # Number of magnetic atoms
         # Atom positions in cartesian coordinates (units of 1/eV)
@@ -103,18 +112,21 @@ class MagnonMaterial(Material):
                 for atom in hamiltonian.magnetic_atoms
             ]
         )
-        # Spins
-        self.sqrt_spins_2 = np.sqrt(np.array([atom.spin for atom in hamiltonian.magnetic_atoms])/2)
-        self.rj = self.dispersion.u # The vectors for rotating to local coordiante system
+        # sqrt(Sj/2)
+        self.sqrt_spins_2 = np.sqrt(
+            np.array([atom.spin for atom in hamiltonian.magnetic_atoms]) / 2
+        )
+        # The vectors for rotating to local coordiante system
+        self.rj = self.dispersion.u
 
         positions = np.array([a.position for a in hamiltonian.magnetic_atoms])
         lattice = hamiltonian.cell * const.Ang_to_inveV
         species = [a.type for a in hamiltonian.magnetic_atoms]
         structure = Structure(lattice, species, positions)
 
-        m_atoms = [m_cell/n_atoms]*n_atoms
+        m_atoms = [m_cell / n_atoms] * n_atoms
         super().__init__(name, structure, None, None, None, m_atoms)
-    
+
     def get_eig(self, k, G):
         """
         k: single k-point, cartesian coordinates (units of eV)
@@ -125,30 +137,32 @@ class MagnonMaterial(Material):
 
         # See Tanner's Disseration and RadTools doc for explanation of the 1/2
         N = self.n_atoms
-        omega_nu_k, Tk = self._get_omega_T(self.dispersion.h(k)/2) 
-        Uk_conj = np.conjugate(Tk[:N, :N]) # This is U_{j,nu,k})*
-        V_minusk = np.conjugate(Tk[N:, :N]) # This is ((V_{j,nu,-k})*)*
+        omega_nu_k, Tk = self._get_omega_T(self.dispersion.h(k) / 2)
+        Uk_conj = np.conjugate(Tk[:N, :N])  # This is U_{j,nu,k})*
+        V_minusk = np.conjugate(Tk[N:, :N])  # This is ((V_{j,nu,-k})*)*
 
-        epsilon_nu_k_G = (prefactor[:, None] * V_minusk).T @ np.conjugate(self.rj) + (prefactor[:, None] * Uk_conj).T @ self.rj
+        epsilon_nu_k_G = (prefactor[:, None] * V_minusk).T @ np.conjugate(self.rj) + (
+            prefactor[:, None] * Uk_conj
+        ).T @ self.rj
 
         return omega_nu_k, epsilon_nu_k_G  # n, and nx3 array of complex numbers
 
-    def _get_omega_T(self,D):
+    def _get_omega_T(self, D):
         """
         D: grand dynamical matrix (2N x 2N)
         """
         N = self.n_atoms
-        g = np.diag([1]*N + [-1]*N)
+        g = np.diag([1] * N + [-1] * N)
 
         # We want D = K^dagger K whereas numpy uses K K^dagger
         K = np.conjugate(np.linalg.cholesky(D)).T
         L, U = np.linalg.eig(K @ g @ np.conjugate(K).T)
 
         # Arrange so that the first N are positive
-        # And last N are negatives of the first N 
+        # And last N are negatives of the first N
         # (see Eq. I4 in Tanner's Dissertation)
         sort_order = np.argsort(L)[::-1]
-        sort_order = np.concatenate((sort_order[:N], sort_order[2*N:N-1:-1]))
+        sort_order = np.concatenate((sort_order[:N], sort_order[2 * N : N - 1 : -1]))
         U = U[:, sort_order]
         L = np.diag(L[sort_order])
 
@@ -156,7 +170,7 @@ class MagnonMaterial(Material):
         #                    omega_1, omega_2, ..., omega_N])
         # See Eq. (I4) in Tanner's Dissertation
         E = g @ L
-        omega = 2*np.diag(np.real(E[:N])) 
+        omega = 2 * np.diag(np.real(E[:N]))
         T = np.linalg.inv(K) @ U @ np.sqrt(E)
 
         return omega, T
@@ -164,23 +178,28 @@ class MagnonMaterial(Material):
 
 # TODO: c_dict and c_dict_form should prob just be merged?
 class Model:
-    def __init__(self, name, c_dict, c_dict_form, m_chi=None, times=None, Fmed_power=0, power_V=0, s_chi=0.5):
+    def __init__(self, name, c_dict, c_dict_form, Fmed_power=0, power_V=0, s_chi=0.5):
         """
         name: string
-        m_chi: list of floats, DM masses (eV)
-        times: list of floats, time of day for calculating earth velocity vector
         Fmed_power: float, negative power of q in the Fmed term
         power_V: float, power of q in the V term (for special mesh)
         s_chi float, spin of DM particle
         """
         self.name = name
 
-        if m_chi is None:
-            m_chi = np.logspace(3, 7, 50)
-        self.m_chi = m_chi
-        if times is None:
-            times = [0]
-        self.times = times
         self.Fmed_power = Fmed_power
         self.power_V = power_V
         self.s_chi = s_chi
+
+
+class Numerics:
+    def __init__(
+        self,
+        N_abc: np.array | list = [50, 25, 25],
+        power_abc: np.array | list = [2, 1, 1],
+        n_DW_xyz: np.array | list = [20, 20, 20],
+        bin_width: float = 1e-3,
+        q_cut: bool = True,
+        special_mesh: bool = True,
+    ):
+        self.N_abc = np.array(N_abc)
