de2781396baf4f1a2b07ae17355d4b8b4ec1163a
Omar Ashour <omar.ashour22@gmail.com>
2024-02-11 23:28:02 -0800

    Add example materials

---
+++ DARK/materials/VBTS_Magnon.py
@@ -0,0 +1,172 @@
+import numpy as np
+from pymatgen.core.structure import Structure
+from copy import deepcopy
+from DARK import constants as const
+from DARK.core import MagnonMaterial
+
+from radtools import (
+    SpinHamiltonian,
+    ExchangeParameter,
+    Atom,
+    Crystal,
+    Lattice,
+)
+
+
+def get_material():
+    def prepare_MVBT_structure(filename):
+        struct = Structure.from_file(filename)
+        struct.remove_species(["Bi", "Te"])
+        # struct.sites[0].species = struct.sites[0].species + "1"
+        # struct.sites[1].species = struct.sites[0].species + "2"
+        return struct
+
+    def get_MVBT_neighbors(struct, neighbor_cutoff=25):
+        indices = [{"inter": [], "intra": []} for i in range(struct.num_sites)]
+        distances = [{"inter": [], "intra": []} for i in range(struct.num_sites)]
+        R = [{"inter": [], "intra": []} for i in range(struct.num_sites)]
+        z = [{"inter": [], "intra": []} for i in range(struct.num_sites)]
+        for i, current_site in enumerate(struct.sites):
+            neighbors = struct.get_neighbors(current_site, neighbor_cutoff)
+            for n in neighbors:
+                if n.index == i and n.coords[2] == current_site.coords[2]:
+                    # Neighbor is same kind of site and at same z value
+                    key = "intra"
+                elif n.index != i and n.coords[2] != current_site.coords[2]:
+                    # Neighbor is different kind of site and different z value
+                    key = "inter"
+                else:
+                    # This is a neighbor of the same kind of site but from a different layer
+                    # We neglect these
+                    continue
+                indices[i][key].append(n.index)
+                R[i][key].append(n.image)
+                distances[i][key].append(n.distance(current_site, [0, 0, 0]))
+                z[i][key].append(n.coords[2])
+            distance_sort = {k: np.argsort(distances[i][k]) for k in ["inter", "intra"]}
+            for k in ["inter", "intra"]:
+                indices[i][k] = np.array(indices[i][k])[distance_sort[k]]
+                distances[i][k] = np.array(distances[i][k])[distance_sort[k]]
+                R[i][k] = np.array(R[i][k])[distance_sort[k]]
+                z[i][k] = np.array(z[i][k])[distance_sort[k]]
+                neighbor_class = -1
+                prev_d = 0
+                new_indices, new_distances, new_R, new_z = [[]], [], [[]], [[]]
+                for j, d, RR, zz in zip(
+                    indices[i][k], distances[i][k], R[i][k], z[i][k]
+                ):
+                    if ~np.isclose(d, prev_d):
+                        neighbor_class += 1
+                        new_indices.append([])
+                        new_distances.append(d)
+                        new_R.append([])
+                        new_z.append([])
+                    new_indices[neighbor_class].append(j)
+                    new_R[neighbor_class].append(RR)
+                    new_z[neighbor_class].append(zz)
+                    prev_d = d
+                # Get rid of that empty list because this is an atrocious implementation
+                indices[i][k] = new_indices[:-1]
+                distances[i][k] = new_distances[:-1]
+                R[i][k] = new_R[:-1]
+                z[i][k] = new_z[:-1]
+
+        return indices, distances, R, z
+
+    def get_MVBT_hamiltonian(filename, J, spin_direction, spin):
+        struct = prepare_MVBT_structure(filename)
+        atoms = [
+            Atom("V", s.frac_coords, index=i, spin=spin)
+            for i, s in enumerate(struct.sites)
+        ]
+        for a in atoms:
+            a.spin_direction = ((-1) ** a.index) * np.array(spin_direction)
+
+        # Convert everything to eV and inverse eV
+        scaled_struct = deepcopy(struct)
+        scaled_struct.scale_lattice(struct.volume * (const.Ang_to_inveV) ** 3)
+        lattice = Lattice(
+            np.round(scaled_struct.lattice.matrix, decimals=7), standardize=False
+        )
+        crystal = Crystal(lattice, atoms, standardize=False)
+        hamiltonian = SpinHamiltonian(crystal, standardize=False)
+
+        J = {
+            "inter": [ExchangeParameter(iso=JJ) for JJ in J["inter"]],
+            "intra": [ExchangeParameter(iso=JJ) for JJ in J["intra"]],
+        }
+
+        hamiltonian.double_counting = True  # Really not sure about this...
+        hamiltonian.spin_normalized = False  # I don't think it's normalized??
+        hamiltonian.factor = -1 / 2  # Really not sure about this eitehr...
+
+        # Add all pairs
+        all_indices, _, all_R, _ = get_MVBT_neighbors(struct)
+        for current_index, (neighbor_indices, neighbor_R) in enumerate(
+            zip(all_indices, all_R)
+        ):
+            # Now this gives the index of the current site,
+            # and dictionaries with the inter/intra layer neighbors
+            for k in ["inter", "intra"]:
+                for neighbor_class, (class_indices, class_R) in enumerate(
+                    zip(neighbor_indices[k], neighbor_R[k])
+                ):
+                    # now each of these have neighbors in the same class (i.e., same distance away)
+                    for neighbor_index, R in zip(class_indices, class_R):
+                        hamiltonian.add_bond(
+                            atoms[current_index],
+                            atoms[neighbor_index],
+                            tuple(R),
+                            J[k][neighbor_class],
+                        )
+        return hamiltonian
+
+    # hamiltonian = get_YIG_hamiltonian('symm_reduced_YIG.vasp')
+    J = {
+        "inter": np.array(
+            [
+                -0.00037656903765693300,
+                0.00046025104602507400,
+                -0.0030543933054393700,
+                -0.0002928870292887300,
+                0.0025523012552301100,
+                0.0016317991631799000,
+                -0.00020920502092053300,
+                0.00029288702928867400,
+                0.0012133891213388900,
+                -0.002970711297071170,
+                0.00004184100418407060,
+                -0.0006276150627615370,
+                -0.0033054393305439700,
+                -0.0006276150627615370,
+                0.00004184100418407060,
+                0.00004184100418407060,
+                -0.0005439330543933400,
+                0.0001255230125522740,
+            ]
+        )
+        * 1e-3,
+        "intra": np.array(
+            [
+                2.4835425076352400,
+                0.011087866108786600,
+                0.061932773109243700,
+                0.0024686192468619000,
+                -0.0012970711297071400,
+                0.0009623430962342820,
+                -0.0005439330543933400,
+                0.00046025104602507400,
+                -0.0007949790794979370,
+                0.0007112970711296780,
+                -0.000041841004184133,
+                -0.000041841004184133,
+                0,
+            ]
+        )
+        * 1e-3,
+    }
+    spin_direction = [1, 0, 0]
+    spin = 3 / 2
+    hamiltonian = get_MVBT_hamiltonian("data/VBTS.vasp", J, spin_direction, spin)
+    m_cell = 2749.367e9  # YIG mass, all ions
+    return MagnonMaterial("VBTS", hamiltonian, m_cell)
---
+++ DARK/materials/YIG_Magnon.py
@@ -0,0 +1,103 @@
+import numpy as np
+from radtools import SpinHamiltonian, Crystal, Lattice, Atom, ExchangeParameter
+from pymatgen.core.structure import Structure
+
+import DARK.constants as const
+from DARK.core import MagnonMaterial
+from copy import deepcopy
+
+
+def get_material():
+    def prepare_YIG_structure(filename):
+        struct = Structure.from_file(filename)
+        struct.remove_species(["Y", "O"])
+        for i in range(12):
+            struct.sites[i].species = "Fe1"
+        for j in range(12, 20):
+            struct.sites[j].species = "Fe2"
+        return struct
+
+    def get_YIG_hamiltonian(filename):
+        struct = prepare_YIG_structure(filename)
+        site_type = lambda i: "d" if i < 12 else "a"
+        # See blue notebook notes page 31 regarding why this is frac_coords
+        # Even though documentation says it should be cartesian ("absolute")
+        atoms = [
+            Atom(f"Fe_{site_type(i)}", s.frac_coords, index=i + 1, spin=5 / 2)
+            for i, s in enumerate(struct.sites)
+        ]
+
+        for atom in atoms:
+            if atom.index < 13:
+                atom.spin_direction = [0, 0, 1]
+            else:
+                atom.spin_direction = [0, 0, -1]
+
+        # Convert everything to eV and inverse eV
+        scaled_struct = deepcopy(struct)
+        scaled_struct.scale_lattice(struct.volume * (const.Ang_to_inveV) ** 3)
+
+        # Having to pass standardize = False every round because of inheritance is a bit absurd
+        lattice = Lattice(
+            np.round(scaled_struct.lattice.matrix, decimals=7), standardize=False
+        )
+        crystal = Crystal(lattice, atoms, standardize=False)
+        hamiltonian = SpinHamiltonian(crystal, standardize=False)
+
+        kelvin_to_eV = 8.617333262145 * 1e-5
+
+        # From that paper cited by Tanner
+        Jad = ExchangeParameter(iso=-40 * kelvin_to_eV)
+        Jdd = ExchangeParameter(iso=-13.4 * kelvin_to_eV)
+        Jaa = ExchangeParameter(iso=-3.8 * kelvin_to_eV)
+        J = {"aa": Jaa, "dd": Jdd, "ad": Jad, "da": Jad}
+
+        hamiltonian.double_counting = False
+        hamiltonian.spin_normalized = False  # I don't think it's normalized??
+        hamiltonian.factor = -2
+
+        # Add all pairs
+        all_indices, all_distances, all_R = get_YIG_neighbors(struct)
+        for current_index, (neighbor_indices, neighbor_R) in enumerate(
+            zip(all_indices, all_R)
+        ):
+            for neighbor_index, R in zip(neighbor_indices, neighbor_R):
+                parameter_type = site_type(current_index) + site_type(neighbor_index)
+                hamiltonian.add_bond(
+                    atoms[current_index],
+                    atoms[neighbor_index],
+                    tuple(R),
+                    J[parameter_type],
+                )
+
+        return hamiltonian
+
+    def get_YIG_neighbors(struct, neighbor_cutoff=5.5):
+        """
+        Returns the indices, distances, and R vectors of the nearest neighbors
+        neighbor_cutoff: distance in Angstroms
+        """
+        indices = []
+        distances = []
+        R = []
+        for current_site in struct.sites:
+            neighbors = struct.get_neighbors(current_site, neighbor_cutoff)
+            neighbor_indices = [n.index for n in neighbors]
+            neighbor_distance = [n.distance(current_site) for n in neighbors]
+            neighbor_R = [
+                n.frac_coords - n.to_unit_cell().frac_coords for n in neighbors
+            ]
+            distance_sort = np.argsort(neighbor_distance)
+            neighbor_indices = np.array(neighbor_indices)[distance_sort]
+            neighbor_distance = np.array(neighbor_distance)[distance_sort]
+            neighbor_R = np.array(neighbor_R)[distance_sort]
+            indices.append(neighbor_indices)
+            distances.append(neighbor_distance)
+            R.append(neighbor_R)
+            # Save results
+
+        return indices, distances, R
+
+    hamiltonian = get_YIG_hamiltonian("data/YIG.vasp")
+    m_cell = 2749.367e9  # YIG mass, all ions
+    return MagnonMaterial("YIG", hamiltonian, m_cell)
