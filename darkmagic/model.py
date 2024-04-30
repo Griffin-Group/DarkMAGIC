86c66e450301caecfa0fd302a756f111f63b22fe
Omar Ashour <omar.ashour22@gmail.com>
2024-04-30 13:50:48 -0700

    Preliminary phonon pot

--- darkmagic/model.py
+++ darkmagic/model.py
@@ -6,8 +6,8 @@ import warnings
 
 SUPPORTED_OPERATORS = {
     "1",
-    "2",
-    "3",
+    "3a",
+    "3b",
     "4",
     "5a",
     "5b",
@@ -52,124 +52,144 @@ class Model:
         self.Fmed_power = Fmed_power
         self.power_V = power_V
         self.S_chi = S_chi
-        # self.coeff_prefactor = coeff_prefactor
-        # self.coeff_func = coeff_func  # Needs better name
-        self.operators, self.particles = self.get_operators_and_particles(
-            coeff_prefactor, coeff_func
+        self.coeff_prefactor = coeff_prefactor
+        self.coeff_func = coeff_func
+        self.operators, self.particles = self._get_operators_and_particles()
+        self._validate_coefficients()
+
+    def get_unscreened_coeff(self, alpha, psi, grid, m_chi, S_chi):
+        return self.coeff_prefactor[alpha][psi] * self.coeff_func[alpha][psi](
+            grid, m_chi, S_chi
         )
 
-        def c(op_id, particle_id, q, m_chi, S_chi):
-            return coeff_prefactor[op_id][particle_id] * coeff_func[op_id][particle_id](
-                q, m_chi, S_chi
-            )
-
-        self.c = c
-
-    def screen_coeff(self, q, m_chi, epsilon):
+    def compute_screened_coeff(self, grid, epsilon, m_chi, S_chi):
         """
         Screen the coefficients by the form factor
         """
-        q2_qepsq = np.linalg.norm(q, axis=1) ** 2 / np.einsum(
-            "ij,jk,ik->i", q, epsilon, q
-        )
-        screened_coeff_qmS = {alpha: {} for alpha in self.operators}
-        for alpha, c_alpha in self.coeff_prefactor.items():
-            screened_c = {
-                "e": q2_qepsq * c_alpha.get("e", 0),
-                "n": c_alpha.get("n", 0),
-                "p": c_alpha.get("p", 0) + (1 - q2_qepsq) * c_alpha.get("e", 0),
-            }
+        q_eps_q = np.sum(grid.qhat_qhat * epsilon[None, :], axis=(-1, -2))
+        screened_coeff = {
+            alpha: {psi: self.coeff_prefactor[alpha][psi] for psi in self.particles}
+            for alpha in self.operators
+        }
+        for alpha, c_alpha in screened_coeff.items():
             for psi in c_alpha.keys():
-                screened_coeff_qmS[alpha][psi] = screened_c[psi] * self.coeff_func(
-                    alpha, psi, q, m_chi, self.S_chi
-                )
+                if psi == "e":
+                    c_alpha[psi] *= 1 / q_eps_q
+                elif psi == "p":
+                    # TODO: is this correct?
+                    c_alpha[psi] += (1 - 1 / q_eps_q) * c_alpha.get("e", 0)
+                elif psi == "n":
+                    c_alpha[psi] *= np.ones_like(q_eps_q)
+                # neutrons are unscreened
+                c_alpha[psi] *= self.coeff_func[alpha][psi](grid, m_chi, S_chi)
 
-    @staticmethod
-    def get_operators_and_particles(coeff_prefactor, coeff_func):
+        return screened_coeff
+
+    def _get_operators_and_particles(self):
         """
-        Gets the non-zero operators and particles (psi) from the c_dict
+        Gets the non-zero operators and particles (psi) from the coefficients
         """
 
         nonzero_pairs = [
             (alpha, psi)
-            for alpha, c_alpha in coeff_prefactor.items()
+            for alpha, c_alpha in self.coeff_prefactor.items()
             for psi, c_psi in c_alpha.items()
             if c_psi != 0
         ]
         operators = {pair[0] for pair in nonzero_pairs}
         particles = {pair[1] for pair in nonzero_pairs}
+
+        return operators, particles
+
+    def _validate_coefficients(self):
+        """
+        Validates the coefficients. Namely, this means that the coefficient functions
+        are defined for every (particle,operator) pair with a non-zero coefficient prefactor, and that every operator is supported.
+
+        Raises:
+            MissingCoefficientFunctionException: If an operator has a non-zero coefficient prefactor but no coefficient function.
+            UnsupportedOperatorException: If an operator is not supported.
+
+        Warns:
+            ExtraCoefficientFunctionWarning: If an operator has a coefficient function but no non-zero coefficient prefactor, so the operator will be ignored.
+        """
         # Check that every operator is supported
-        for alpha in operators:
+        for alpha in self.operators:
             if alpha not in SUPPORTED_OPERATORS:
                 raise UnsupportedOperatorException(
                     f"Operator {alpha} is not supported."
                 )
         # Ignore operators that have coefficient function defined but
         # no non-zero coefficient prefactor
-        for alpha in list(coeff_func.keys()):
-            if alpha not in operators:
+        for alpha in list(self.coeff_func.keys()):
+            if alpha not in self.operators:
                 warnings.warn(
                     f"Operator {alpha} has a coefficient function defined but no "
                     "corresponding nonzero coefficient prefactor for any particle. "
                     "It will be ignored.",
                     ExtraCoefficientFunctionWarning,
                 )
-                del coeff_func[alpha]
+                del self.coeff_func[alpha]
                 continue
-            for psi in list(coeff_func[alpha].keys()):
-                if psi not in particles:
+            for psi in list(self.coeff_func[alpha].keys()):
+                if psi not in self.particles:
                     warnings.warn(
                         f"Operator {alpha} has a coefficient function defined for "
                         f"particle {psi} but no corresponding nonzero coefficient "
                         "prefactor. It will be ignored.",
                         ExtraCoefficientFunctionWarning,
                     )
-                    del coeff_func[alpha][psi]
+                    del self.coeff_func[alpha][psi]
         # Check that every operator with a non-zero coefficient prefactor has
         # a coefficient function defined
-        if operators != set(coeff_func.keys()):
+        if self.operators != set(self.coeff_func.keys()):
             raise MissingCoefficientFunctionException(
                 "Some operators with non-zero coefficient prefactors have "
                 "no coefficient functions defined."
             )
         particles_from_func = {
-            key for alpha in coeff_func for key in coeff_func[alpha].keys()
+            key for alpha in self.coeff_func for key in self.coeff_func[alpha].keys()
         }
-        if particles != particles_from_func:
+        if self.particles != particles_from_func:
             raise MissingCoefficientFunctionException(
                 "Some particles with non-zero coefficient prefactors "
                 "have no coefficient functions defined."
             )
-
-        return operators, particles
 
 
 class Potential:
     def __init__(self, model):
         self.operators = model.operators
         self.particles = model.particles
-        self.coeff_prefactor = model.coeff_prefactor
-        self.coeff_func = model.coeff_func
+        self.c = model.compute_screened_coeff
 
-    def eval_V(self, q, material, m_chi, S_chi):
-        # TODO: expand this to work for arrays of q (needs to change all the way down)
+    def eval_V(self, grid, material, m_chi, S_chi):
+        """
+        Evaluate the full potential V_j(q) for the given grid and material
+        """
         full_V = self._get_full_V()
-        expansions = {key for alpha in self.operators for key in full_V[alpha].keys()}
+        terms = {key for alpha in self.operators for key in full_V[alpha].keys()}
 
         # Determine which velocity integrals are needed
-        self.needs_g1 = bool("01" or "10" in expansions)
-        self.needs_g2 = "11" in expansions
+        self.needs_g1 = bool("12" or "11" in terms)
+        self.needs_g2 = "20" in terms
 
-        V = {exp_id: self.get_zeros(exp_id, material.n_atoms) for exp_id in expansions}
+        V = {t: self._get_zeros(t, material.n_atoms, grid) for t in terms}
+
+        def get_slice(t):
+            return (slice(None),) + (None,) * (V[t].ndim - 1)
+
+        coeff = self.c(grid, material.epsilon, m_chi, S_chi)
         # TODO: write this nicer
         for psi in self.particles:
             for alpha in self.operators:
-                C = self.coeff_prefactor[alpha][psi] * self.coeff_func(
-                    alpha, psi, q, m_chi, S_chi
-                )
-                for exp_id, V_func in full_V[alpha].items():
-                    # print(f"V^({psi})_{alpha}_{exp_id}")
-                    V[exp_id] += C * V_func(q, psi, material, m_chi, S_chi)
+                C = coeff[alpha][psi]
+                for t, V_func in full_V[alpha].items():
+                    # print(f"V^({psi})_{alpha}_{t}")
+                    # print(V[t].shape)
+                    # print(V_func(grid, psi, material, m_chi, S_chi).shape)
+                    # print(C.shape)
+                    V[t] += C[get_slice(t)] * V_func(grid, psi, material, m_chi, S_chi)
         return V
 
     @classmethod
@@ -183,7 +203,7 @@ class Potential:
             getattr(cls, method) for method in dir(cls) if method.startswith("V")
         ]
 
-        # Extract ids from the name (e.g. V3b_10 -> 3b, 10)
+        # Extract term type from the name (e.g. V3b_10 -> 3b, 10)
         operators = [method.__name__.split("_") for method in methods]
         operators = [(op[0].lstrip("V"), op[1]) for op in operators]
 
@@ -193,28 +213,34 @@ class Potential:
         return V
 
     @staticmethod
-    def get_zeros(exp_id, n_atoms):
-        if exp_id == "00":
-            return np.zeros(n_atoms, dtype=complex)
-        elif exp_id in ["01", "10"]:
-            return np.zeros((n_atoms, 3), dtype=complex)
-        elif exp_id == "11":
-            return np.zeros((n_atoms, 3, 3), dtype=complex)
+    def _get_zeros(term, n_atoms, grid):
+        nq = len(grid.q_cart)
+        if term == "00":
+            return np.zeros((nq, n_atoms), dtype=complex)
+        elif term in ["11", "12"]:
+            return np.zeros((nq, n_atoms, 3), dtype=complex)
+        elif term == "20":
+            return np.zeros((nq, n_atoms, 3, 3), dtype=complex)
+        else:
+            raise ValueError(f"Unknown term type {term}")
 
     # TODO: Rewrite all of the Vs in terms of qhat
     # TODO: add option to specify L \times S and/or L \dot S in input
     #       and reconstruct the relevant portions of L \otimes S form them?
     @staticmethod
-    def V1_00(q, psi, material, m_chi, S_chi):
-        return material.properties.N[psi]
+    def V1_00(grid, psi, material, m_chi, S_chi):
+        # NOTE: ported to grid
+        return material.properties.N[psi] * np.ones(
+            (grid.q_cart.shape[0], material.n_atoms)
+        )
 
     @staticmethod
-    def V3b_00(q, psi, material, m_chi, S_chi):
+    def V3b_00(grid, psi, material, m_chi, S_chi):
         V = np.zeros(material.n_atoms, dtype=complex)
 
-        qhat = q / np.linalg.norm(q)
+        qhat = grid.q_hat
 
-        C = -0.5 * np.linalg.norm(q) ** 2 / material.properties.m_psi[psi] ** 2
+        C = -0.5 * grid.q_norm**2 / material.properties.m_psi[psi] ** 2
 
         for j in range(material.n_atoms):
             LxS_V = material.properties.L_tens_S[psi][j]
@@ -224,86 +250,97 @@ class Potential:
         return V
 
     @staticmethod
-    def V3a_12(q, psi, material, m_chi, S_chi):
+    def V3a_12(grid, psi, material, m_chi, S_chi):
         C = 1j / material.properties.m_psi[psi]
 
-        return np.array([C * np.cross(Sj, q) for Sj in material.properties.S[psi]])
+        return C * np.cross(material.properties.S[psi], grid.q_cart)
+        # return np.array(
+        #    [C * np.cross(Sj, grid.q_cart) for Sj in material.properties.S[psi]]
+        # )
 
     @staticmethod
-    def V4_11(q, psi, material, m_chi, S_chi):
-        return material.properties.S[psi]
+    def V4_11(grid, psi, material, m_chi, S_chi):
+        return material.properties.S[psi][:, None, None] * np.ones(
+            (grid.q_cart.shape[0], 1)
+        )
 
     @staticmethod
-    def V5b_11(q, psi, material, m_chi, S_chi):
-        C = -0.5 * np.dot(q, q) * (material.properties.m_psi[psi]) ** (-2)
+    def V5b_11(grid, psi, material, m_chi, S_chi):
+        C = -0.5 * grid.q_norm**2 * (material.properties.m_psi[psi]) ** (-2)
 
-        qhat = q / np.linalg.norm(q)
+        qhat = grid.q_hat
 
         return C * np.array(
             [L - qhat * np.dot(qhat, L) for L in material.properties.L[psi]]
         )
 
     @staticmethod
-    def V5a_20(q, psi, material, m_chi, S_chi):
+    def V5a_20(grid, psi, material, m_chi, S_chi):
         C = 1j / material.properties.m_psi[psi]
 
         return C * np.array(
             [
-                N * np.einsum("jki,k->ij", levi_civita, q)
+                N * np.einsum("jki,k->ij", levi_civita, grid.q_cart)
                 for N in material.properties.N[psi]
             ]
         )
 
     @staticmethod
-    def V6_11(q, psi, material, m_chi, S_chi):
-        C = np.dot(q, q) * (material.properties.m_psi[psi]) ** (-2)
+    def V6_11(grid, psi, material, m_chi, S_chi):
+        C = grid.q_norm**2 * (material.properties.m_psi[psi]) ** (-2)
 
-        qhat = q / np.linalg.norm(q)
+        qhat = grid.q_hat
 
         return C * np.array(
             [qhat * np.dot(qhat, S) for S in material.properties.S[psi]]
         )
 
     @staticmethod
-    def V7a_00(q, psi, material, m_chi, S_chi):
+    def V7a_00(grid, psi, material, m_chi, S_chi):
         C = -(0.5) * (m_chi) ** (-1.0)
 
-        return C * np.array([np.dot(q, S) for S in material.properties.S[psi]])
+        return C * np.array(
+            [np.dot(grid.q_cart, S) for S in material.properties.S[psi]]
+        )
 
     @staticmethod
-    def V7b_00(q, psi, material, m_chi, S_chi):
+    def V7b_00(grid, psi, material, m_chi, S_chi):
         C = -(0.5) * (m_chi) ** (-1.0) * 1j
 
         return C * np.array(
             [
-                np.einsum("ijk,ij,k->", levi_civita, LxS, q)
+                np.einsum("ijk,ij,kl->k", levi_civita, LxS, grid.q_cart)
                 for LxS in material.properties.L_tens_S[psi]
             ]
         )
 
     @staticmethod
-    def V8a_11(q, psi, material, m_chi, S_chi):
-        C = -q / 2 / m_chi
+    def V8a_11(grid, psi, material, m_chi, S_chi):
+        C = -grid.q_cart / 2 / m_chi
 
-        return C * np.array(list(material.properties.N[psi]))
+        return C * np.array(list(material.properties.N[psi]))[:, None]
 
     @staticmethod
-    def V8a_12(q, psi, material, m_chi, S_chi):
+    def V8a_20(grid, psi, material, m_chi, S_chi):
         C = 1
 
         return C * np.array([N * np.identity(3) for N in material.properties.N[psi]])
 
     @staticmethod
-    def V8b_01(q, psi, material, m_chi, S_chi):
+    def V8b_11(grid, psi, material, m_chi, S_chi):
         C = 0.5 * (1j / material.properties.m_psi[psi])
 
-        return C * np.array([np.cross(L, q) for L in material.properties.L[psi]])
+        return C * np.array(
+            [np.cross(L, grid.q_cart) for L in material.properties.L[psi]]
+        )
 
     @staticmethod
-    def V9_11(q, psi, material, m_chi, S_chi):
+    def V9_11(grid, psi, material, m_chi, S_chi):
         C = 1j / material.properties.m_psi[psi]
 
-        return C * np.array([np.cross(S, q) for S in material.properties.S[psi]])
+        return C * np.array(
+            [np.cross(S, grid.q_cart) for S in material.properties.S[psi]]
+        )
 
     @staticmethod
     def V10_00(q, psi, material, m_chi, S_chi):
@@ -318,7 +355,7 @@ class Potential:
         return C * np.array([N * q for N in material.properties.N[psi]])
 
     @staticmethod
-    def V12a_01(q, psi, material, m_chi, S_chi):
+    def V12a_11(q, psi, material, m_chi, S_chi):
         C = -m_chi / 2
 
         return C * np.array([np.cross(S, q) for S in material.properties.S[psi]])
@@ -368,45 +405,59 @@ class Potential:
         )
 
     @staticmethod
-    def V14a_11(q, psi, material, m_chi, S_chi):
-        C = 1j / material.properties.m_psi[psi]
-
-        return C * np.array(
-            [np.einsum("i,j->ij", S, q) for S in material.properties.S[psi]]
-        )
-
-    @staticmethod
-    def V14b_11(q, psi, material, m_chi, S_chi):
-        C = 0.5 * material.properties.m_psi[psi] ** (-2)
+    def V14a_11(grid, psi, material, m_chi, S_chi):
+        C = -1j / (2 * material.properties.m_psi[psi] * m_chi)
 
         return C * np.array(
             [
-                np.einsum("ijk,ij,k->", levi_civita, LxS, q) * q
-                for LxS in material.properties.L_tens_S[psi]
-            ]
-        )
-
-    @staticmethod
-    def V15a_20(q, psi, material, m_chi, S_chi):
-        C = -(material.properties.m_psi[psi] ** (-2))
-
-        return C * np.array(
-            [
-                np.dot(q, S) * np.einsum("jki,k->ij", levi_civita, q)
+                np.einsum("i,j,k->i", grid.q_cart, S, grid.q_cart)
                 for S in material.properties.S[psi]
             ]
         )
 
     @staticmethod
-    def V15b_11(q, psi, material, m_chi, S_chi):
+    def V14a_20(grid, psi, material, m_chi, S_chi):
+        C = 1j / material.properties.m_psi[psi]
+
+        return C * np.array(
+            [np.einsum("i,j->ij", S, grid.q_cart) for S in material.properties.S[psi]]
+        )
+
+    @staticmethod
+    def V14b_11(grid, psi, material, m_chi, S_chi):
+        C = 0.5 * material.properties.m_psi[psi] ** (-2)
+
+        return C * np.array(
+            [
+                np.einsum("ijk,ij,k->", levi_civita, LxS, grid.q_cart) * grid.q_cart
+                for LxS in material.properties.L_tens_S[psi]
+            ]
+        )
+
+    @staticmethod
+    def V15a_20(grid, psi, material, m_chi, S_chi):
+        C = -(material.properties.m_psi[psi] ** (-2))
+
+        return C * np.array(
+            [
+                np.dot(grid.q_cart, S)
+                * np.einsum("jki,k->ij", levi_civita, grid.q_cart)
+                for S in material.properties.S[psi]
+            ]
+        )
+
+    @staticmethod
+    def V15b_11(grid, psi, material, m_chi, S_chi):
         C = -0.5 * 1j * material.properties.m_psi[psi] ** (-3)
 
+        # LxS = material.properties.L_tens_S[psi]
+        # M1 = (LxS @ grid.q_cart[..., None]).squeeze()
         return (
             C
-            * np.dot(q, q)
+            * grid.q_norm**2
             * np.array(
                 [
-                    LxS @ q - q * (q @ LxS @ q)
+                    LxS @ grid.q_cart - grid.q_cart * (grid.q_cart @ LxS @ grid.q_cart)
                     for LxS in material.properties.L_tens_S[psi]
                 ]
             )
